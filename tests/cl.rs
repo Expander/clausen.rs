d6a3694ef389f1ae8a66587c212abfc19fdaf58f
Alexander Voigt <alexander.voigt@hs-flensburg.com>
2023-11-14 16:44:36 +0100

    adding loop over test data

--- tests/cl.rs
+++ tests/cl.rs
@@ -1,6 +1,14 @@
 extern crate clausen;
+extern crate num;
+use num::Float;
+mod common;
 
 #[test]
 fn test_values() {
-    assert!(true);
+    let eps = 1e-14;
+    let values = common::read_data_file("Cl1.txt").unwrap();
+
+    for &(_v, _expected) in values.iter() {
+        assert_eq_float!(0.0, 0.0, eps);
+    }
 }
