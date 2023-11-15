072cdac90d17c74511b9271a241ce4b4580f47af
Alexander Voigt <ahgvoigt@gmail.com>
2023-11-15 17:43:03 +0100

    benchmark Cl(1,x)

--- benches/cl.rs
+++ benches/cl.rs
@@ -1,14 +1,27 @@
 use clausen::Cl;
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::*;
 
-fn bench_cl2(c: &mut Criterion) {
-    c.bench_function("cl(2, -0.1)", |b| b.iter(|| black_box(-0.1).cl(2)));
-    c.bench_function("cl(2, -3.2)", |b| b.iter(|| black_box(-3.2).cl(2)));
-    c.bench_function("cl(2, -6.5)", |b| b.iter(|| black_box(-6.5).cl(2)));
-    c.bench_function("cl(2,  0.1)", |b| b.iter(|| black_box( 0.1).cl(2)));
-    c.bench_function("cl(2,  3.2)", |b| b.iter(|| black_box( 3.2).cl(2)));
-    c.bench_function("cl(2,  6.5)", |b| b.iter(|| black_box( 6.5).cl(2)));
+fn bench_cl1(c: &mut Criterion) {
+    let mut group = c.benchmark_group("cl(1,x)");
+    group.bench_function("x=-0.1", |b| b.iter(|| black_box(-0.1).cl(1)));
+    group.bench_function("x=-3.2", |b| b.iter(|| black_box(-3.2).cl(1)));
+    group.bench_function("x=-6.5", |b| b.iter(|| black_box(-6.5).cl(1)));
+    group.bench_function("x=0.1" , |b| b.iter(|| black_box( 0.1).cl(1)));
+    group.bench_function("x=3.2" , |b| b.iter(|| black_box( 3.2).cl(1)));
+    group.bench_function("x=6.5" , |b| b.iter(|| black_box( 6.5).cl(1)));
+    group.finish();
 }
 
-criterion_group!(benches, bench_cl2);
+fn bench_cl2(c: &mut Criterion) {
+    let mut group = c.benchmark_group("cl(2,x)");
+    group.bench_function("x=-0.1", |b| b.iter(|| black_box(-0.1).cl(2)));
+    group.bench_function("x=-3.2", |b| b.iter(|| black_box(-3.2).cl(2)));
+    group.bench_function("x=-6.5", |b| b.iter(|| black_box(-6.5).cl(2)));
+    group.bench_function("x=0.1" , |b| b.iter(|| black_box( 0.1).cl(2)));
+    group.bench_function("x=3.2" , |b| b.iter(|| black_box( 3.2).cl(2)));
+    group.bench_function("x=6.5" , |b| b.iter(|| black_box( 6.5).cl(2)));
+    group.finish();
+}
+
+criterion_group!(benches, bench_cl1, bench_cl2);
 criterion_main!(benches);
