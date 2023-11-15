0cd3668685dffb46d68e888fbed56c8b62a81fb6
Alexander Voigt <ahgvoigt@gmail.com>
2023-11-15 17:25:10 +0100

    shorten function call

--- benches/cl.rs
+++ benches/cl.rs
@@ -1,17 +1,13 @@
 use clausen::Cl;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-fn cl2(x: f64) {
-    x.cl(2);
-}
-
 fn bench_cl2(c: &mut Criterion) {
-    c.bench_function("cl(2, -0.1)", |b| b.iter(|| cl2(black_box(-0.1))));
-    c.bench_function("cl(2, -3.2)", |b| b.iter(|| cl2(black_box(-3.2))));
-    c.bench_function("cl(2, -6.5)", |b| b.iter(|| cl2(black_box(-6.5))));
-    c.bench_function("cl(2,  0.1)", |b| b.iter(|| cl2(black_box( 0.1))));
-    c.bench_function("cl(2,  3.2)", |b| b.iter(|| cl2(black_box( 3.2))));
-    c.bench_function("cl(2,  6.5)", |b| b.iter(|| cl2(black_box( 6.5))));
+    c.bench_function("cl(2, -0.1)", |b| b.iter(|| black_box(-0.1).cl(2)));
+    c.bench_function("cl(2, -3.2)", |b| b.iter(|| black_box(-3.2).cl(2)));
+    c.bench_function("cl(2, -6.5)", |b| b.iter(|| black_box(-6.5).cl(2)));
+    c.bench_function("cl(2,  0.1)", |b| b.iter(|| black_box( 0.1).cl(2)));
+    c.bench_function("cl(2,  3.2)", |b| b.iter(|| black_box( 3.2).cl(2)));
+    c.bench_function("cl(2,  6.5)", |b| b.iter(|| black_box( 6.5).cl(2)));
 }
 
 criterion_group!(benches, bench_cl2);
