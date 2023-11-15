683135bc218602b4dd81163e9499848524d9bb11
Alexander Voigt <ahgvoigt@gmail.com>
2023-11-15 07:05:04 +0100

    adding Cl(2,x)

--- src/range_reduction.rs
+++ src/range_reduction.rs
@@ -15,3 +15,26 @@ pub fn range_reduce_odd(mut x: f64) -> f64 {
 
     x
 }
+
+
+pub fn range_reduce_even(mut x: f64) -> (f64, f64) {
+    let mut sgn = 1.0;
+
+    if x < 0.0 {
+        x = -x;
+        sgn = -1.0;
+    }
+
+    if x >= 2.0*std::f64::consts::PI {
+        x = x.rem_euclid(2.0*std::f64::consts::PI);
+    }
+
+    if x > std::f64::consts::PI {
+        let p0 = 6.28125;
+        let p1 = 0.0019353071795864769253;
+        x = (p0 - x) + p1;
+        sgn = -sgn;
+    }
+
+    (x, sgn)
+}
--- tests/cl.rs
+++ tests/cl.rs
@@ -6,10 +6,12 @@ mod common;
 #[test]
 fn test_values() {
     let eps = 1e-14;
-    let n = 1;
-    let values = common::read_data_file(&format!("Cl{}.txt", n)).unwrap();
 
-    for &(v, expected) in values.iter() {
-        assert_eq_float!(v.cl(n), expected, eps);
+    for n in [1, 2].iter() {
+        let values = common::read_data_file(&format!("Cl{}.txt", *n)).unwrap();
+
+        for &(v, expected) in values.iter() {
+            assert_eq_float!(v.cl(*n), expected, eps);
+        }
     }
 }
