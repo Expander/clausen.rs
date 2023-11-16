e9a271d701d026c9793f5db32dee1e45938b9eac
Alexander Voigt <alexander.voigt@hs-flensburg.com>
2023-11-16 10:38:27 +0100

    implement Cl_n(x) for n >= 10

--- src/cln.rs
+++ src/cln.rs
@@ -10,7 +10,39 @@ pub fn cln(n: i64, x: f64) -> f64 {
     } else if n < 10 {
         sgn*0.0 // @todo(alex): implement advanced algorithm
     } else {
-        sgn*0.0 // @todo(alex): implement series expansion
+        sgn*cln_series(n, r)
+    }
+}
+
+
+/// Series expansion of Cl_n(x)
+fn cln_series(n: i64, x: f64) -> f64 {
+    let kmax = (f64::EPSILON.powf(-(n as f64).recip())).ceil() as i64;
+
+    if is_even(n) {
+        let (mut si, co) = x.sin_cos();
+        let mut si2 = 0.0; // sin((n-2)*x)
+        let mut si1 = si;  // sin((n-1)*x)
+        let mut sum = si;
+        for k in 2..=kmax {
+            si = 2.0*co*si1 - si2; // sin(n*x)
+            si2 = si1;
+            si1 = si;
+            sum += si/(k as f64).powf(n as f64);
+        }
+        sum
+    } else {
+        let co = x.cos();
+        let mut co2 = 1.0; // cos((n-2)*x)
+        let mut co1 = co;  // cos((n-1)*x)
+        let mut sum = co;
+        for k in 2..=kmax {
+            let con = 2.0*co*co1 - co2; // cos(n*x)
+            co2 = co1;
+            co1 = con;
+            sum += con/(k as f64).powf(n as f64);
+        }
+        sum
     }
 }
 
--- tests/cl.rs
+++ tests/cl.rs
@@ -11,7 +11,7 @@ fn test_non_positive_order() {
 fn test_values() {
     let eps = 1e-14;
 
-    for n in [1, 2, 3, 4, 5, 6].iter() {
+    for n in [1, 2, 3, 4, 5, 6, 10, 11, 12, 13, 14, 15, 16, 1000, 1001, 1000000].iter() {
         let values = common::read_data_file(&format!("Cl{}.txt", *n)).unwrap();
 
         for &(v, expected) in values.iter() {
