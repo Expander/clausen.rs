7a1fd337e6c6de058563ae9a5dfaf31a64ff1f89
Alexander Voigt <ahgvoigt@gmail.com>
2023-11-16 09:23:42 +0100

    adding general range reduction

--- src/cln.rs
+++ src/cln.rs
@@ -1,4 +1,18 @@
+use crate::range_reduction::{range_reduce_even, range_reduce_odd};
+
+
 /// Standard Clausen function Cl_n(x) for a real argument
-pub fn cln(_n: i64, _x: f64) -> f64 {
+pub fn cln(n: i64, x: f64) -> f64 {
+    let (r, sgn) = range_reduce(n, x);
+
     0.0
+}
+
+
+fn range_reduce(n: i64, x: f64) -> (f64, f64) {
+    if n % 2 == 0 {
+        range_reduce_even(x)
+    } else {
+        (range_reduce_odd(x), 1.0)
+    }
 }
