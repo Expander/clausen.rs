930db41e4c819cd8b67a7cbd7069dd7ed7dcce04
Alexander Voigt <ahgvoigt@gmail.com>
2023-11-16 18:11:57 +0100

    benchmark Cl_n(x) for n = 7 and 8

--- benches/cl.rs
+++ benches/cl.rs
@@ -23,6 +23,18 @@ fn bench_cl2(c: &mut Criterion) {
     group.finish();
 }
 
+fn bench_cl7(c: &mut Criterion) {
+    let mut group = c.benchmark_group("cl(7,x)");
+    group.bench_function("x=1.0", |b| b.iter(|| black_box(1.0).cl(7)));
+    group.finish();
+}
+
+fn bench_cl8(c: &mut Criterion) {
+    let mut group = c.benchmark_group("cl(8,x)");
+    group.bench_function("x=1.0", |b| b.iter(|| black_box(1.0).cl(8)));
+    group.finish();
+}
+
 fn bench_cl9(c: &mut Criterion) {
     let mut group = c.benchmark_group("cl(9,x)");
     group.bench_function("x=1.0", |b| b.iter(|| black_box(1.0).cl(9)));
@@ -35,5 +47,5 @@ fn bench_cl10(c: &mut Criterion) {
     group.finish();
 }
 
-criterion_group!(benches, bench_cl1, bench_cl2, bench_cl9, bench_cl10);
+criterion_group!(benches, bench_cl1, bench_cl2, bench_cl7, bench_cl8, bench_cl9, bench_cl10);
 criterion_main!(benches);
