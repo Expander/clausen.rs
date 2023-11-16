abf3282c9196bb666fd161426a55e589d2d68e3d
Alexander Voigt <ahgvoigt@gmail.com>
2023-11-16 09:28:01 +0100

    implement cases for chosing between different algorithms for cl(n,x)

--- src/cln.rs
+++ src/cln.rs
@@ -5,14 +5,25 @@ use crate::range_reduction::{range_reduce_even, range_reduce_odd};
 pub fn cln(n: i64, x: f64) -> f64 {
     let (r, sgn) = range_reduce(n, x);
 
-    0.0
+    if is_even(n) && (r == 0.0 || r == core::f64::consts::PI) {
+        0.0
+    } else if n < 10 {
+        sgn*0.0 // @todo(alex): implement advanced algorithm
+    } else {
+        sgn*0.0 // @todo(alex): implement series expansion
+    }
 }
 
 
 fn range_reduce(n: i64, x: f64) -> (f64, f64) {
-    if n % 2 == 0 {
+    if is_even(n) {
         range_reduce_even(x)
     } else {
         (range_reduce_odd(x), 1.0)
     }
+}
+
+
+fn is_even(n: i64) -> bool {
+    n % 2 == 0
 }
