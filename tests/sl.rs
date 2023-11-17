3c9f302cd770ef59168695949cc0dc8b742e5976
Alexander Voigt <ahgvoigt@gmail.com>
2023-11-17 16:56:02 +0100

    testing Sl_n(x) for n=1..31

--- tests/sl.rs
+++ tests/sl.rs
@@ -11,7 +11,7 @@ fn test_non_positive_order() {
 fn test_values() {
     let eps = 1e-14;
 
-    for n in [1, 2, 3, 4, 1000, 1001].iter() {
+    for n in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 1000, 1001].iter() {
         let values = common::read_data_file(&format!("Sl{}.txt", *n)).unwrap();
 
         for &(v, expected) in values.iter() {
