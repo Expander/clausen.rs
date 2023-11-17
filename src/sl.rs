9f82e93737f161458a91121ab3247b7cae3fd25f
Alexander Voigt <ahgvoigt@gmail.com>
2023-11-17 07:55:14 +0100

    create a function for Sl_n(x)

--- src/sl.rs
+++ src/sl.rs
@@ -15,7 +15,12 @@ impl Sl<f64> for f64 {
     fn sl(&self, n: i64) -> f64 {
         match n {
             k if k < 1 => panic!("sl(n) not implemented for n < 1 (you've called {}.sl({}))", self, n),
-            _ => 0.0
+            _ => sln(n, *self)
         }
     }
+}
+
+
+fn sln(n: i64, x: f64) -> f64 {
+    0.0
 }
