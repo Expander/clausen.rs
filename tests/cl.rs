2a6febf3c7d01917f0164722e47230a6bcf17557
Alexander Voigt <ahgvoigt@gmail.com>
2023-11-22 07:21:01 +0100

    adding test for complex input values z to Cl_n(z)

--- tests/cl.rs
+++ tests/cl.rs
@@ -1,4 +1,5 @@
 use num::complex::Complex;
+use num::Float;
 use clausen::Cl;
 mod common;
 
@@ -15,6 +16,25 @@ fn test_values() {
             assert_eq_float!(Complex::new(v, 0.0).cl(*n).re, expected, eps);
         }
     }
+}
+
+
+#[test]
+fn test_complex_input() {
+    let eps = 1e-14;
+
+    assert_eq_complex!(Complex::new(1.0,  1.0).cl(-2), Complex::new( 0.50688457710655124,  0.50950616274374456), eps);
+    assert_eq_complex!(Complex::new(1.0, -1.0).cl(-2), Complex::new( 0.50688457710655124, -0.50950616274374456), eps);
+    assert_eq_complex!(Complex::new(1.0,  1.0).cl(-1), Complex::new(-0.08267495282794197,  0.49171277760817954), eps);
+    assert_eq_complex!(Complex::new(1.0, -1.0).cl(-1), Complex::new(-0.08267495282794197, -0.49171277760817954), eps);
+    // assert_eq_complex!(Complex::new(1.0,  1.0).cl( 0), Complex::new(0.5, 0.5).cot()/2.0, eps);
+    // assert_eq_complex!(Complex::new(1.0, -1.0).cl( 0), Complex::new(0.5, 0.5).coth()*Complex::<f64>::i()/2.0, eps);
+    assert_eq_complex!(Complex::new(0.0,  1.0).cl( 1), Complex::new(-0.0413248546129181, -1.5707963267948966), eps);
+    assert_eq_complex!(Complex::new(0.0, -1.0).cl( 1), Complex::new(-0.0413248546129181, -1.5707963267948966), eps);
+    assert_eq_complex!(Complex::new(0.0,  1.0).cl( 2), Complex::new( 1.5707963267948966,  0.9861797794993302), eps);
+    assert_eq_complex!(Complex::new(0.0, -1.0).cl( 2), Complex::new(-1.5707963267948966, -0.9861797794993302), eps);
+    assert_eq_complex!(Complex::new(1.0,  1.0).cl( 2), Complex::new( 1.4107754938116412, -0.1044778629291566), eps);
+    assert_eq_complex!(Complex::new(1.0, -1.0).cl( 2), Complex::new( 1.4107754938116412,  0.1044778629291566), eps);
 }
 
 
